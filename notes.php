746d04f6241e3884ea46c0fdbeed8ccc7b7a09e3
raveendra <ravindra.m568@gmail.com>
2022-04-12 10:52:12 +0530

    Commit

---
+++ notes.php
@@ -0,0 +1,9 @@
+git init
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ravindraff/Practice-React.git
+git push -u origin main
+git push
+git push --set-upstream origin main
+
+git remote add origin https://github.com/ravindraff/Practice-React.git
